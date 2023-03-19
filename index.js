a7090a07dc2b5e19aa19d2f0c69366a99c5e9f28
Konuralp <konuralpcelikyay@gmail.com>
2023-03-19 21:25:02 +0100

    Text location adjusted and color changed

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const container = document.getElementById("container");
 
 let temperature = 0;
 
+let da = new Date();
+
+
+
 let weather = "";
 
 const giphyAPI = "hJbjgBVp70ZI2v0EQJCL4bFOg3k4PLy9";
@@ -24,11 +28,11 @@ tiles.addTo(map);
 
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '9944b9fa1cmshcfea24cb5c9348ep1df2d1jsn5c2976d1f13d',
-		'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '9944b9fa1cmshcfea24cb5c9348ep1df2d1jsn5c2976d1f13d',
+    'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
+  }
 };
 
 let inputValue = "";
@@ -40,30 +44,39 @@ function getInputValue(){
 
 
 search.addEventListener("click", async function(){
-    getInputValue();
-    getWeather();
-    
-    
-    
 
-  
+  getInputValue();
+  getWeather();
+
+
+
+
+
 
 });
 
 
 async function getWeather(){
   try{
-  const response = await fetch(`https://forecast9.p.rapidapi.com/rapidapi/forecast/${inputValue}/hourly/`, options);
-  const data = await response.json();
-  temperature = data.forecast.items[16].temperature.avg;
-  weather =  data.forecast.items[16].weather.text;
-  console.log(weather);
-  renderGif();
-  const lat = data.location.coordinates.latitude;
-  const longi = data.location.coordinates.longitude;
-  //L.marker([lat,longi ]).addTo(map);
-  map.setView([lat,longi],10)
-  degree.textContent = `In ${inputValue} is right now ${temperature} Celsius`;
+    const response = await fetch(`https://forecast9.p.rapidapi.com/rapidapi/forecast/${inputValue}/hourly/`, options);
+    const data = await response.json();
+    temperature = data.forecast.items[da.getHours()+1].temperature.avg;
+    weather =  data.forecast.items[da.getHours()+1].weather.text;
+    console.log(weather);
+    renderGif();
+    const lat = data.location.coordinates.latitude;
+    const longi = data.location.coordinates.longitude;
+    //L.marker([lat,longi ]).addTo(map);
+    map.setView([lat,longi],10)
+    degree.textContent = `In ${inputValue} is right now ${temperature} Celsius`;
+    degree.style.cssText = `font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-weight: bolder;
+  background-color: rgb(236, 191, 132);
+  box-shadow: 50px 8px 15px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  width: 200px;
+  margin-left: 50px;`
+
   }catch(error){
     degree.textContent = "Too many Requests";
 
@@ -74,10 +87,10 @@ async function getWeather(){
 
 async function renderGif(){
   try{
-    
-    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${giphyAPI}&q=${weather}&limit=25&offset=0&rating=g&lang=en`);
+
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${giphyAPI}&q=${weather}&limit=25&offset=23&rating=g&lang=en`);
     const gifData = await response.json();
-    const url = gifData.data[0].images.downsized.url
+    const url = gifData.data[2].images.downsized.url
     container.innerHTML = `
     <p id="container">
     <img id="gif" src="${url}" alt="gif">
@@ -87,7 +100,7 @@ async function renderGif(){
     `
 
 
-  
+
 
 
   }catch(error){
